5d406cae00eb7e414ba80b20fd74a04fc2decddc
akrecev <akrecev@gmail.com>
2025-02-23 18:21:29 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,118 @@
+import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
+import name.remal.gradle_plugins.sonarlint.SonarLintExtension
+import org.gradle.plugins.ide.idea.model.IdeaLanguageLevel
+import org.springframework.boot.gradle.plugin.SpringBootPlugin
+
+plugins {
+    java
+    idea
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    id("name.remal.sonarlint")
+    id("com.diffplug.spotless")
+}
+
+group = "ru.kretsev"
+version = "0.0.1"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+idea {
+    project {
+        languageLevel = IdeaLanguageLevel(21)
+    }
+    module {
+        isDownloadJavadoc = true
+        isDownloadSources = true
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencyManagement {
+    dependencies {
+        imports {
+            mavenBom(SpringBootPlugin.BOM_COORDINATES)
+        }
+    }
+}
+
+val springdocOpenapiVersion: String by project
+val jsonwebtokenVersion: String by project
+val mapstructVersion: String by project
+val lombokMapstructBindingVersion: String by project
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springdocOpenapiVersion")
+    implementation("org.mapstruct:mapstruct:$mapstructVersion")
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("org.postgresql:postgresql")
+    annotationProcessor("org.mapstruct:mapstruct-processor:$mapstructVersion")
+    annotationProcessor("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok-mapstruct-binding:$lombokMapstructBindingVersion")
+
+    implementation("io.jsonwebtoken:jjwt-api:$jsonwebtokenVersion")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:$jsonwebtokenVersion")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:$jsonwebtokenVersion")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.compilerArgs.addAll(listOf("-Xlint:all,-serial,-processing"))
+
+    dependsOn("spotlessApply")
+}
+apply<name.remal.gradle_plugins.sonarlint.SonarLintPlugin>()
+configure<SonarLintExtension> {
+    nodeJs {
+        detectNodeJs = false
+        logNodeJsNotFound = false
+    }
+}
+apply<com.diffplug.gradle.spotless.SpotlessPlugin>()
+configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+    java {
+        palantirJavaFormat("2.39.0")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging.showExceptions = true
+    reports {
+        junitXml.required.set(true)
+        html.required.set(true)
+    }
+}
+
+tasks {
+    val managedVersions by registering {
+        doLast {
+            project.extensions.getByType<DependencyManagementExtension>()
+                    .managedVersions
+                    .toSortedMap()
+                    .map { "${it.key}:${it.value}" }
+                    .forEach(::println)
+        }
+    }
+}
+
---
+++ src/main/java/ru/kretsev/TaskManagementApp.java
@@ -0,0 +1,11 @@
+package ru.kretsev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagementApp.class, args);
+    }
+}
