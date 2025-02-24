2b77be072f8362d0df2aa5eb9485690c995fefd9
akrecev <akrecev@gmail.com>
2025-02-24 20:10:01 +0300

    add Swagger documentation for auth api

--- src/main/java/ru/kretsev/auth/AuthenticationController.java
+++ src/main/java/ru/kretsev/auth/AuthenticationController.java
@@ -1,5 +1,9 @@
 package ru.kretsev.auth;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,14 +17,27 @@ import ru.kretsev.dto.user.RegisterRequest;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@Tag(name = "Аутентификация", description = "Методы для регистрации и входа в систему")
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
+    @Operation(summary = "Регистрация нового пользователя", description = "Создаёт нового пользователя в системе")
+    @ApiResponses(
+            value = {
+                @ApiResponse(responseCode = "200", description = "Пользователь успешно зарегистрирован"),
+                @ApiResponse(responseCode = "400", description = "Ошибка валидации входных данных")
+            })
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(authenticationService.register(request));
     }
 
+    @Operation(summary = "Аутентификация пользователя", description = "Возвращает JWT-токен при успешной авторизации")
+    @ApiResponses(
+            value = {
+                @ApiResponse(responseCode = "200", description = "Успешная аутентификация"),
+                @ApiResponse(responseCode = "403", description = "Неверные учетные данные")
+            })
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
         return ResponseEntity.ok(authenticationService.authenticate(request));
