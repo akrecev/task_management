d1efa72c90875015407bf1d9136a4cef80b4350a
akrecev <akrecev@gmail.com>
2025-02-26 12:01:16 +0300

    add repositories

--- src/main/java/ru/kretsev/repository/TaskRepository.java
+++ src/main/java/ru/kretsev/repository/TaskRepository.java
@@ -1,15 +1,12 @@
 package ru.kretsev.repository;
 
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.kretsev.model.user.Role;
+import ru.kretsev.model.task.Task;
 import ru.kretsev.model.user.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-
-    List<User> findByRole(Role role);
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByAuthor(User author);
 }
