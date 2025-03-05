783f7dda451343b13055a216ec0b47fdff5413af
akrecev <akrecev@gmail.com>
2025-03-05 23:13:00 +0300

    add logging for exceptions handling

--- src/main/java/ru/kretsev/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/kretsev/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package ru.kretsev.exception;
 import jakarta.persistence.EntityNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -25,33 +28,40 @@ public class GlobalExceptionHandler {
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
         });
+
+        log.warn("Ошибка валидации данных: {}", errors);
         return ResponseEntity.badRequest().body(errors);
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
         if (ex.getMessage() != null && ex.getMessage().contains("users_email_key")) {
+            log.warn("Email уже используется другим пользователем: {}", ex.getMessage());
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Email уже используется другим пользователем.");
         }
 
+        log.error("Нарушение целостности данных: {}", ex.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body("Нарушение целостности данных: " + ex.getMessage());
     }
 
     @ExceptionHandler(AccessDeniedException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ResponseEntity<String> handleAccessDeniedException(AccessDeniedException e) {
+        log.warn("Доступ запрещен: {}", e.getMessage());
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException e) {
+        log.error("Данные отсутсвуют: {}", e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<String> handleAllExceptions(Exception e) {
+        log.error("Ошибка выполнения программы: {}", e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("Произошла внутренняя ошибка сервера \n" + e.getMessage());
     }
